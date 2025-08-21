73276790b892420f8a77575dcbe6d16869d51937
CarlosVillarinho <worksvillarinho@gmail.com>
2025-08-21 11:36:17 -0300

    Enviando Projeto Finalizado

--- src/main/java/br/fiap/conexao/Main.java
+++ src/main/java/br/fiap/conexao/Main.java
@@ -4,7 +4,6 @@ import br.fiap.modelo.Categoria;
 import br.fiap.modelo.CategoriaDAO;
 import br.fiap.modelo.Despesa;
 import br.fiap.modelo.DespesaDAO;
-import sun.security.krb5.internal.crypto.Des;
 
 import java.util.List;
 
@@ -14,7 +13,6 @@ public class Main {
         //System.out.println(Conexao.conectar());
 
         CategoriaDAO dao = new CategoriaDAO();
-        DespesaDAO dao1 = new DespesaDAO();
 
         //CRIA NOVOS ELEMENTOS
         //Categoria categoria = new Categoria(3L, "educaçao");
@@ -25,14 +23,9 @@ public class Main {
         //dao.inserir(categoriaL);
 
         //SELECT, MOSTRA A TABELA COM OS ELEMENTOS
-        //List<Categoria> Lista = dao.listar();
-        //for (Categoria categoria : Lista){
-            //System.out.println(categoria.getId() + " ---> " + categoria.getCategoria());
-        //}
-
-        List<Despesa> Lista1 = dao1.listar();
-        for (Despesa despesa : Lista1) {
-            System.out.println(despesa.getId() + " ---> " + despesa.getDescricao() + " ---> " +despesa.getValor() + despesa.getCategoria() + " ---> " + despesa.getData() + " ---> ");
+        List<Categoria> Lista = dao.listar();
+        for (Categoria categoria : Lista){
+            System.out.println(categoria.getId() + " ---> " + categoria.getCategoria());
         }
     }
 }
--- src/main/java/br/fiap/conexao/TesteDespesa.java
+++ src/main/java/br/fiap/conexao/TesteDespesa.java
@@ -6,6 +6,7 @@ import br.fiap.modelo.DespesaDAO;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class TesteDespesa {
     public static void main(String[] args) {
@@ -15,21 +16,30 @@ public class TesteDespesa {
         //FORMATA O MODELO DA DATA
         DateTimeFormatter mascara = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        Despesa despesa = new Despesa();
-        //ADICIONANDO ELEMENTOS NA TABELA
-        despesa.setId(102L);
-        despesa.setDescricao("Lanche na FIAP");
-        despesa.setValor(45.00);
-        despesa.setCategoria(new Categoria(4l, ""));
-        despesa.setData(LocalDate.parse("21/08/2025", mascara));
-        dao.inserir(despesa);
+//        Despesa despesa = new Despesa();
+//        //ADICIONANDO ELEMENTOS NA TABELA
+//        despesa.setId(102L);
+//        despesa.setDescricao("Lanche na FIAP");
+//        despesa.setValor(45.00);
+//        despesa.setCategoria(new Categoria(4l, ""));
+//        despesa.setData(LocalDate.parse("21/08/2025", mascara));
+//        dao.inserir(despesa);
 
         //ADICIONANDO MAIS ELEMENTOS NA TABELA
-        despesa.setId(103L);
-        despesa.setDescricao("Monster para assistir aula de Java");
-        despesa.setValor(20.00);
-        despesa.setCategoria(new Categoria(3l, ""));
-        despesa.setData(LocalDate.parse("21/08/2025", mascara));
-        dao.inserir(despesa);
+//        despesa.setId(103L);
+//        despesa.setDescricao("Monster para assistir aula de Java");
+//        despesa.setValor(20.00);
+//        despesa.setCategoria(new Categoria(3l, ""));
+//        despesa.setData(LocalDate.parse("21/08/2025", mascara));
+//        dao.inserir(despesa);
+
+        List<Despesa> Lista = dao.listar();
+        for (Despesa despesa : Lista) {
+            System.out.println(despesa.getId());
+            System.out.println(despesa.getDescricao());
+            System.out.println(despesa.getValor());
+            System.out.println(despesa.getData());
+            System.out.println(despesa.getCategoria().getId());
+        }
     }
 }
