31f6d18998e7ccab8bb6e126d67bdb71745d7a13
Carlos Villarinho <worksvillarinho@gmail.com>
2025-08-28 10:22:22 -0300

    Update Operações SQL.sql

--- Operações SQL.sql
+++ Operações SQL.sql
@@ -25,8 +25,29 @@ commit;
 --DELETA UM ELEMENTO PERMANEMTEMENTE -------------------------------------------
 delete from java_categoria where id_categoria = 2;
 
+--DELETA TODA A TABELA PERMANENTEMENTE------------------------------------------
+drop table java_categoria;
+drop table java_despesa;
+
 --VOLTA OPERAÇÃO----------------------------------------------------------------
 rollback;
 
 --ALTERA UM ELEMENTO INSERIDO, PARA NAO DELETAR E INSERIR DE NOVO---------------
 update java_categoria set categoria = 'xyz' where id_categoria = 2;
+
+--SELECT QUE APRESENTA APENAS O QUE EU QUERO -----------------------------------
+select d.descricao, d.valor, d.data, c.categoria            
+from java_despesa d                                      --nao tem probelma-----
+inner join java_categoria c                              --em pular as linhas---
+on d.id_categoria = c.id_categoria;                      --tudo é um só comando-
+
+--DELETA TODA A SEQUENCIA PERMANENTEMENTE---------------------------------------
+drop sequence seqc;
+drop sequence seqd;
+
+--CRIANDO UMA NOVA SEQUENCIA----------------------------------------------------
+create SEQUENCE seqc start WITH 10 INCREMENT by 1;
+create SEQUENCE seqd start WITH 1000 INCREMENT by 1;
+
+--INSERINDO UM NOVO ELEMENTO EM SEQUENCIA---------------------------------------
+insert into java_categoria values (seqc.nextval, 'educação');
